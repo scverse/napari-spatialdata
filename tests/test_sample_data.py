49726bbec5efd742305f01c590a48f796e11c359
giovp <giov.pll@gmail.com>
2022-04-04 18:12:16 +0200

    remove test files from cookiecutter

--- tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from napari_spatial_anndata import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
--- tests/test_writer.py
+++
@@ -1,7 +0,0 @@
-# from napari_spatial_anndata import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
