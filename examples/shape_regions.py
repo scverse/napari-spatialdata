14db5f2656bf9d614a4c33cd2aa897a43b843e3a
Ganesh <vcganesh_95@yahoo.in>
2022-08-15 11:50:09 +0200

    add scalebar (#18)
    * add scalebar
    
    * update scale and second example
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: ganesh <ganesh.chandrasekaran@tum.de>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- examples/shape_regions.py
+++ examples/shape_regions.py
@@ -6,11 +6,11 @@ img1 = adata.uns["spatial"]["V1_Adult_Mouse_Brain"]["images"]["hires"].copy()
 
 viewer = napari.Viewer()
 viewer.add_image(
-    img1,
-    rgb=True,
-    name="image1",
-    metadata={"adata": adata, "library_id": "V1_Adult_Mouse_Brain"},
+    img1, rgb=True, name="image1", metadata={"adata": adata, "library_id": "V1_Adult_Mouse_Brain"}, scale=(1, 1)
 )
+
+viewer.scale_bar.visible = True
+viewer.scale_bar.unit = "um"
 
 if __name__ == "__main__":
     napari.run()
