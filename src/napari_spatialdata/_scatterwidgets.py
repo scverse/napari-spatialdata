3312ee77c1cc705bc842e4c2980f2338bf12987c
Rahul Shrestha <rahulshrestha0101@gmail.com>
2022-10-14 16:51:23 +0200

    Added lassoselector class

--- src/napari_spatialdata/_scatterwidgets.py
+++ src/napari_spatialdata/_scatterwidgets.py
@@ -1,3 +1,4 @@
+
 from __future__ import annotations
 
 from typing import Any, Union, Iterable, Optional, TYPE_CHECKING
@@ -13,11 +14,76 @@ from napari_spatialdata._model import ImageModel
 from napari_spatialdata._utils import NDArrayA, _get_categorical
 from napari_spatialdata._widgets import AListWidget, ComponentWidget
 
+from matplotlib.widgets import LassoSelector, PolygonSelector
+from matplotlib.path import Path
+
 __all__ = [
     "MatplotlibWidget",
     "AxisWidgets",
 ]
 
+class SelectFromCollection:
+    """
+    Select indices from a matplotlib collection using `LassoSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        Axes to interact with.
+    collection : `matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to *alpha_other*.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection.get_offsets()
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        self.fc = collection.get_facecolors()
+        if len(self.fc) == 0:
+            raise ValueError('Collection must have a facecolor')
+        elif len(self.fc) == 1:
+            self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.selector = LassoSelector(ax, onselect=self.onselect)
+        #self.selector = PolygonSelector(ax, self.onselect)
+
+        self.ind = []
+
+    
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        self.fc[:, -1] = self.alpha_other
+        self.fc[self.ind, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+                
+        # TODO: (1) store indicies to anndata (2) color selected points separately
+
+        logger.debug(self.ind)
+
+    def disconnect(self):
+        self.selector.disconnect_events()
+        self.poly.disconnect_events()
+        self.fc[:, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
 
 class ScatterListWidget(AListWidget):
     attrChanged = Signal()
@@ -132,7 +198,14 @@ class MatplotlibWidget(NapariMPLWidget):
         color_label: Optional[str],
     ) -> None:
 
-        self.axes.scatter(x=x_data, y=y_data, c=color_data, alpha=0.5)
+    
+        self.points = self.axes.scatter(x=x_data, y=y_data, c=color_data, alpha=0.5)
+        
+        logger.debug("Points: {}", self.points)
+        logger.debug("X axis: {}", x_data)
+        logger.debug("Y axis: {}", y_data)
+
+        self.selector = SelectFromCollection(self.axes, self.points)
         self.axes.set_xlabel(x_label)
         self.axes.set_ylabel(y_label)
 
