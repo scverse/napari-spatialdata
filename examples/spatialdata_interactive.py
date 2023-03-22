68f1862580e4688f61572b4270165fc7aa496ddb
Rahul Shrestha <rahulshrestha0101@gmail.com>
2023-03-22 13:41:05 +0100

    precommit fix

--- examples/spatialdata_interactive.py
+++ examples/spatialdata_interactive.py
@@ -1,10 +1,8 @@
-import napari
 from spatialdata import SpatialData
 from napari_spatialdata._interactive import Interactive
 
-if __name__ == "__main__": 
-
+if __name__ == "__main__":
     sdata = SpatialData.read("../data/cosmx/data.zarr")
     sdata.table.uns["spatialdata_attrs"]["region"] = 0
-    i = Interactive(sdata) 
+    i = Interactive(sdata)
     i.run()
