896c6ac61bfa7176a317527d866d5ca97f591139
wmv_hpomen <w-mv@hotmail.com>
2023-06-30 11:07:59 +0200

    Add test

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -11,7 +11,7 @@ def test_layer_visibility(qtbot, make_napari_viewer: any):
     widget = SdataWidget(viewer, sdata)
 
     # Click on `space` coordinate system
-    center_pos = get_center_pos_listitem(widget.coordinate_system_widget, "space")
+    center_pos = get_center_pos_listitem(widget.coordinate_system_widget, "global")
     click_list_widget_item(qtbot, widget.coordinate_system_widget, center_pos, "currentItemChanged")
 
     widget._onClick(list(sdata.images.keys())[0])
@@ -22,3 +22,30 @@ def test_layer_visibility(qtbot, make_napari_viewer: any):
     assert viewer.layers[0].metadata["sdata"] == new_metadata["sdata"]
 
     Viewer.close_all()
+
+
+def test_metadata_inheritance(qtbot, make_napari_viewer: any):
+    viewer = make_napari_viewer()
+    widget = SdataWidget(viewer, sdata)
+
+    # Click on `global` coordinate system
+    center_pos = get_center_pos_listitem(widget.coordinate_system_widget, "space")
+    click_list_widget_item(qtbot, widget.coordinate_system_widget, center_pos, "currentItemChanged")
+
+    widget._add_image(list(sdata.images.keys())[0])
+    widget._add_image(list(sdata.images.keys())[1])
+    widget.viewer_model.viewer.add_shapes()
+
+    # Two layers have the same spatialdata object. So we should count 1 spatialdata object.
+    layers = widget.viewer_model.viewer.layers
+    sdatas = [layer.metadata["sdata"] for layer in layers if "sdata" in layer.metadata]
+    sdata_count = sum(sdatas[0] is sdata_blob for sdata_blob in sdatas[1:]) if len(sdatas) != 1 else len(sdatas)
+    assert sdata_count == 1
+
+    widget.viewer_model.inherit_metadata(widget.viewer_model.viewer.layers)
+
+    # Now we did let the shapes layer inherit sdata from another layer. The number of unique spatialdata objects
+    # should still be one.
+    sdatas = [layer.metadata["sdata"] for layer in layers if "sdata" in layer.metadata]
+    sdata_count = sum(sdatas[0] is sdata_blob for sdata_blob in sdatas[1:]) if len(sdatas) != 1 else len(sdatas)
+    assert sdata_count == 1
