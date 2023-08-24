b275998901cb9158fb31f202e637cd23b76f3530
wmv_hpomen <w-mv@hotmail.com>
2023-08-24 17:49:12 +0200

    Implement temporary fix add_element

--- src/napari_spatialdata/_interactive.py
+++ src/napari_spatialdata/_interactive.py
@@ -7,7 +7,7 @@ import shapely
 from napari.utils.events import EventedList
 
 from napari_spatialdata._sdata_widgets import SdataWidget
-from napari_spatialdata.utils._utils import NDArrayA
+from napari_spatialdata.utils._utils import NDArrayA, _get_sdata_key, get_duplicate_element_names
 
 if TYPE_CHECKING:
     from spatialdata import SpatialData
@@ -30,25 +30,40 @@ class Interactive:
     """
 
     def add_element(self, coordinate_system_name: str, element: str) -> SpatialData:
-        for element_type, element_name, _ in self._sdata.filter_by_coordinate_system(
-            coordinate_system_name
-        )._gen_elements():
-            if element_name == element:
-                if element_type == "images":
-                    self._sdata_widget.viewer_model.add_sdata_image(self._sdata, coordinate_system_name, element)
-                elif element_type == "labels":
-                    self._sdata_widget.viewer_model.add_sdata_labels(self._sdata, coordinate_system_name, element)
-                elif element_type == "points":
-                    self._sdata_widget.viewer_model.add_sdata_points(self._sdata, coordinate_system_name, element)
-                elif element_type == "shapes":
-                    if type(self._sdata.shapes[element].iloc[0][0]) == shapely.geometry.point.Point:
-                        self._sdata_widget.viewer_model.add_sdata_circles(self._sdata, coordinate_system_name, element)
-                    elif (type(self._sdata.shapes[element].iloc[0][0]) == shapely.geometry.polygon.Polygon) or (
-                        type(self._sdata.shapes[element].iloc[0][0]) == shapely.geometry.multipolygon.MultiPolygon
-                    ):
-                        self._sdata_widget.viewer_model.add_sdata_shapes(self._sdata, coordinate_system_name, element)
+        elements = {}
+        duplicate_element_names, _ = get_duplicate_element_names(self._sdata)
 
-                break
+        for index, sdata in enumerate(self._sdata):
+            for element_type, element_name, _ in sdata.filter_by_coordinate_system(
+                coordinate_system_name
+            )._gen_elements():
+                elements_metadata = {
+                    "element_type": element_type,
+                    "sdata_index": index,
+                    "original_name": element_name,
+                }
+                name = element_name if element_name not in duplicate_element_names else element_name + f"_{index}"
+                if element_name == element:
+                    name_to_add = name
+                elements[name] = elements_metadata
+
+        sdata, multi = _get_sdata_key(self._sdata, elements, element_name)
+        metadata = elements.get(name_to_add)
+        if metadata:
+            element_type = metadata["element_type"]
+            if element_type == "images":
+                self._sdata_widget.viewer_model.add_sdata_image(sdata, coordinate_system_name, name_to_add, multi)
+            elif element_type == "labels":
+                self._sdata_widget.viewer_model.add_sdata_labels(sdata, coordinate_system_name, name_to_add, multi)
+            elif element_type == "points":
+                self._sdata_widget.viewer_model.add_sdata_points(sdata, coordinate_system_name, name_to_add, multi)
+            elif element_type == "shapes":
+                if type(sdata.shapes[element].iloc[0][0]) == shapely.geometry.point.Point:
+                    self._sdata_widget.viewer_model.add_sdata_circles(sdata, coordinate_system_name, name_to_add, multi)
+                elif (type(sdata.shapes[element].iloc[0][0]) == shapely.geometry.polygon.Polygon) or (
+                    type(sdata.shapes[element].iloc[0][0]) == shapely.geometry.multipolygon.MultiPolygon
+                ):
+                    self._sdata_widget.viewer_model.add_sdata_shapes(sdata, coordinate_system_name, name_to_add, multi)
         else:
             raise ValueError("Element {element_name} not found in coordinate system {coordinate_system_name}.")
 
