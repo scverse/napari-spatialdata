5b600dc83d6d8cfc33c81166e523c914dcf93f5c
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2023-11-06 16:15:14 +0100

    Add element api (#170)
    * create public api for switching cs
    
    * add test
    
    * fix leaking test
    
    * refactor
    
    * Adjust add element API
    
    * add test
    
    * fix leak in test
    
    * Fix pre-commit issue
    
    ---------
    
    Co-authored-by: Luca Marconato <m.lucalmer@gmail.com>

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -7,19 +7,24 @@ from tests.conftest import PlotTester, PlotTesterMeta
 class TestImages(PlotTester, metaclass=PlotTesterMeta):
     def test_plot_can_add_element_image(self, sdata_blobs: SpatialData):
         i = Interactive(sdata=sdata_blobs, headless=True)
-        i.add_element(coordinate_system_name="global", element="blobs_image")
+        i.add_element(element="blobs_image", element_coordinate_system="global")
 
     def test_plot_can_add_element_label(self, sdata_blobs: SpatialData):
         i = Interactive(sdata=sdata_blobs, headless=True)
-        i.add_element(coordinate_system_name="global", element="blobs_labels")
+        i.add_element(element="blobs_labels", element_coordinate_system="global")
 
     def test_plot_can_add_element_multiple(self, sdata_blobs: SpatialData):
         i = Interactive(sdata=sdata_blobs, headless=True)
-        i.add_element(coordinate_system_name="global", element="blobs_image")
-        i.add_element(coordinate_system_name="global", element="blobs_labels")
-        i.add_element(coordinate_system_name="global", element="blobs_circles")
 
-    def test_switch_coorindate_system(self, sdata_blobs: SpatialData):
+        i.add_element(element="blobs_image", element_coordinate_system="global")
+        i.add_element(element="blobs_labels", element_coordinate_system="global")
+        i.add_element(element="blobs_circles", element_coordinate_system="global")
+        assert not i._sdata_widget.coordinate_system_widget._system
+        assert not i._sdata_widget.elements_widget._elements
+        for layer in i._viewer.layers:
+            assert layer.visible
+
+    def test_switch_coordinate_system(self, sdata_blobs: SpatialData):
         i = Interactive(sdata=sdata_blobs, headless=True)
         assert not i._sdata_widget.coordinate_system_widget._system
         assert not i._sdata_widget.elements_widget._elements
@@ -27,3 +32,11 @@ class TestImages(PlotTester, metaclass=PlotTesterMeta):
         assert i._sdata_widget.coordinate_system_widget._system == "global"
         assert i._sdata_widget.elements_widget._elements
         i._viewer.close()
+
+
+def test_plot_can_add_element_switch_cs(sdata_blobs: SpatialData):
+    i = Interactive(sdata=sdata_blobs, headless=True)
+    i.add_element(element="blobs_image", element_coordinate_system="global", view_element_system=True)
+    assert i._sdata_widget.coordinate_system_widget._system == "global"
+    assert i._viewer.layers[-1].visible
+    i._viewer.close()
