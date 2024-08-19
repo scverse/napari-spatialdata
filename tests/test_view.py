060d5d0e145cc4819ba20427436e5a6ab43a03a4
LucaMarconato <2664412+LucaMarconato@users.noreply.github.com>
2024-08-19 20:57:54 +0200

    fix #282 (error in channel selection due to #276) (#301)
    * fix #282 (error in channel selection due to #153)
    
    * cleanup commented code
    
    * fix test slider python 3.11

---
+++ tests/test_view.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+import pytest
+from napari.utils.events import EventedList
+from napari_spatialdata._sdata_widgets import SdataWidget
+from napari_spatialdata._view import QtAdataViewWidget
+from spatialdata.datasets import blobs
+
+
+@pytest.mark.parametrize("widget", [QtAdataViewWidget])
+@pytest.mark.parametrize("n_channels", [3, 5])
+def test_channel_slider_images(qtbot, make_napari_viewer: any, widget: Any, n_channels: int):
+    channels = [f"channel_{i}" for i in range(n_channels)]
+    sdata_blobs = blobs(c_coords=channels)
+    viewer = make_napari_viewer()
+    sdata_widget = SdataWidget(viewer, EventedList([sdata_blobs]))
+
+    viewer.window.add_dock_widget(sdata_widget, name="SpatialData")
+    sdata_widget.viewer_model.add_sdata_image(sdata_blobs, "blobs_image", "global", False)
+
+    # this connects the slider to the viewer (done in __init__)
+    _ = widget(viewer)
+
+    # check if the slider is present
+    start, stop, step = viewer.dims.range[0]
+    assert start == 0
+    assert step == 1
+
+    # simulate position change of the slider
+    viewer.dims.set_current_step(0, 0)
+    qtbot.wait(50)  # wait for a short time to simulate user interaction
+    viewer.dims.set_current_step(0, 1)
+    qtbot.wait(50)  # wait for a short time to simulate user interaction
+
+    viewer.close()
