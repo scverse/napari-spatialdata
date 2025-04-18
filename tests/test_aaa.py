f5c8fd07e94a8a1c0d2c734ff3683e3070c920de
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2025-04-18 11:11:05 +0200

    additional close viewer

--- tests/test_aaa.py
+++ tests/test_aaa.py
@@ -1,4 +1,5 @@
 import pytest
+from napari.viewer import Viewer
 
 from napari_spatialdata._model import DataModel
 from napari_spatialdata._scatterwidgets import PlotWidget
@@ -21,3 +22,4 @@ def test_initialization(plot_widget):
 def test_elementwidget(make_napari_viewer):
     _ = make_napari_viewer()
     assert 1 == 1
+    Viewer.close_all()
