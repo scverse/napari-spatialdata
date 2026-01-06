7f633ae0db07baa4cf678bfc8dfe7d120e16f4c3
Luca Marconato <m.lucalmer@gmail.com>
2026-01-06 15:20:06 +0100

    cleanup

--- src/napari_spatialdata/utils/_viewer_utils.py
+++ src/napari_spatialdata/utils/_viewer_utils.py
@@ -1,27 +1,5 @@
-from typing import cast
-
 from geopandas import GeoDataFrame
 from spatialdata.models import get_axes_names
-
-
-def add_z_to_list_of_xy_tuples(xy: list[tuple[float, float]], z: float) -> list[tuple[float, float, float]]:
-    """
-    Add z coordinates to a list of (x, y) tuples.
-
-    Parameters
-    ----------
-    xy
-        List of (x, y) tuples.
-    z
-        z coordinate to add to each tuple.
-
-    Returns
-    -------
-    list[tuple[float, float, float]]
-        List of (x, y, z) tuples.
-    """
-    return [(x, y, z) for x, y in xy]
-
 
 # type aliases, only used in this module
 Coord2D = tuple[float, float]
@@ -43,14 +21,14 @@ def _get_polygons_properties(df: GeoDataFrame, simplify: bool, include_z: bool) 
         indices.append(int(df.index[i]))
 
         if simplify:
-            xy = cast(list[Coord2D], list(df.geometry.iloc[i].exterior.simplify(tolerance=2).coords))
+            xy = list(df.geometry.iloc[i].exterior.simplify(tolerance=2).coords)
         else:
-            xy = cast(list[Coord2D], list(df.geometry.iloc[i].exterior.coords))
+            xy = list(df.geometry.iloc[i].exterior.coords)
 
         coords: Polygon2D | Polygon3D
         if add_z:
             z_val = float(df.iloc[i].z.item() if hasattr(df.iloc[i].z, "item") else df.iloc[i].z)
-            coords = add_z_to_list_of_xy_tuples(xy=xy, z=z_val)
+            coords = [(x, y, z_val) for x, y in xy]
         else:
             coords = xy
 
